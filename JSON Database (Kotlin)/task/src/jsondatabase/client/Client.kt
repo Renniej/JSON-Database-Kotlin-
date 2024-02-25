721bbf752932d7d80620d8a309c97b011082d9f1
Rennie <ouroboric2023@proton.me>
2024-02-24 21:00:32 -0500

    Finished staged 3/6.... god damn Array<String>

--- JSON Database (Kotlin)/task/src/jsondatabase/client/Client.kt
+++ JSON Database (Kotlin)/task/src/jsondatabase/client/Client.kt
@@ -1,31 +1,33 @@
 package jsondatabase.client
 
 import jsondatabase.requestResponse.Request
+import java.util.*
 
 
-fun createRequestFromArgs(args : Array<String>): Request {
+fun createRequestFromArgs(args : List<String>): Request {
+
 
     val cmd = args[args.indexOf("-t") + 1]
     val index = args[args.indexOf("-i") + 1]
 
+    println("CMD : $cmd ,${cmd.length}")
+
    return when(cmd) {
         "exit" -> Request.of(cmd)
         "set" -> {
-
-
-            val otherData = args.toString().substringAfter("-m ")
-
+            val otherData = args[args.indexOf("-m") + 1]
 
 
             Request.of("$cmd $index $otherData")
         }
         else -> Request.of("$cmd $index")
     }
+
 }
 fun main(args : Array<String>) { //args example : -t set -i 148 -m Here is some text to store on the server
 
 
-    val request = createRequestFromArgs(args)
+    val request = createRequestFromArgs(args.toList())
     val client = ClientManager(address = "127.0.0.1", port = 23456)
 
 
